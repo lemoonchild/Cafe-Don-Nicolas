e4ff882fa2e682378f7f85f43baf2a727c739ed3
XavierLopez25 <lop22716@uvg.edu.gt>
2025-04-22 10:29:48 -0600

    feat: export db connection module

---
+++ backend/src/config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "");
+    console.log("✅ MongoDB conectado");
+  } catch (err) {
+    console.error("❌ Error al conectar a MongoDB", err);
+    process.exit(1); // Exit the process with failure
+  }
+};
