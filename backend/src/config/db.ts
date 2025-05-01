1cc47185b3c7973c5ead0c482267520931f59c3a
lemoonchild <ncastrom21@gmail.com>
2025-05-01 15:00:50 -0600

    feat: adding gridfs config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,27 @@
 import mongoose from "mongoose";
+import { GridFSBucket } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+let gfs: GridFSBucket;
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "");
+    const conn = await mongoose.connect(process.env.MONGO_URI || "");
     console.log("✅ MongoDB conectado");
+
+    const db = conn.connection.db!;
+    gfs = new mongoose.mongo.GridFSBucket(db, { bucketName: "fs" });
+    console.log("📦 GridFS inicializado");
+
   } catch (err) {
     console.error("❌ Error al conectar a MongoDB", err);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   }
+};
+
+export const getGFS = () => {
+  if (!gfs) throw new Error("GridFS no ha sido inicializado aún.");
+  return gfs;
 };
